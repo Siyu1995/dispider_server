9840305e568fdae3329e56621bef065651e63364
siyu <1147701897@qq.com>
2025-07-29 22:09:20 +0800

    baoliu

--- dispider_backend/src/auth/dependencies.py
+++ dispider_backend/src/auth/dependencies.py
@@ -24,9 +24,7 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        print(token)
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-        print(payload)
         username: str = payload.get("sub")
         token_data = schemas.TokenData(username=username)
     except JWTError:
